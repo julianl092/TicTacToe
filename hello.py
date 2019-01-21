f1de7a2c98fb56961ff9d4955ada5d53c72b5acd
julianl092 <36961145+julianl092@users.noreply.github.com>
2019-01-21 15:39:57 -0500

    Index route changed

--- hello.py
+++ hello.py
@@ -10,7 +10,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
  
-@app.route("/index")
+@app.route("/")
 def index():
  
     if "board" not in session:
